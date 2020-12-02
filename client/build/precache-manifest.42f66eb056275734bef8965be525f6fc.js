f9fe60424353daff1e9ca3cba01a52c0add07d8f
Bob <robertamccullough@gmail.com>
2020-12-02 10:51:38 -0700

    add build directories

---
+++ client/build/precache-manifest.42f66eb056275734bef8965be525f6fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6f2698c5024b0e7f44850be7873b5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98c0b0065e55a3b8df4a",
+    "url": "/static/css/main.4ae8b6a4.chunk.css"
+  },
+  {
+    "revision": "b58c71be74557ee025f1",
+    "url": "/static/js/2.85c60068.chunk.js"
+  },
+  {
+    "revision": "98c0b0065e55a3b8df4a",
+    "url": "/static/js/main.1f56d45e.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
